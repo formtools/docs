42523fbed7be756c5f73461b391d904d1a05607d
Ben Keen <ben.keen@gmail.com>
2014-08-09 18:17:03 -0700

    module nav section - helper functions

--- update_gh_pages.sh
+++ update_gh_pages.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+#-----------------------------------------------------------------------------------------------------------------------
+# This is a quick helper script to wipe out the gh-pages branch with the latest from github's master branch.
+#
+# WARNING! Only ever run this script if your master branch has been fully checked in. Otherwise you may lose your
+# changes. You Have Been Warned.
+#-----------------------------------------------------------------------------------------------------------------------
+
 git checkout gh-pages
 git reset --hard origin/master
 git push origin gh-pages
