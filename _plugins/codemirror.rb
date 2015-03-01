ce3f4a9d677d16286bcd469244dc6cdcd310911e
Benjamin Keen <ben.keen@gmail.com>
2015-02-28 20:00:27 -0800

    Codemirror now working

---
+++ _plugins/codemirror.rb
@@ -0,0 +1,21 @@
+# CodeMirror
+#
+# A simple tag for wrapping a code block in a <pre> with a codemirror class and a data-lang attribute. This
+# is sufficient info for the client-side code to highlight the block.
+
+module Jekyll
+	class CodeMirror < Liquid::Block
+
+        def initialize(tag_name, lang, tokens)
+            super
+            @lang = lang.to_s.strip
+        end
+
+		def render(context)
+		    require 'CGI'
+		    '<textarea class="codemirror" data-lang="' + @lang.to_s + '">' + CGI.escapeHTML(super.strip) + '</textarea>'
+		end
+	end
+end
+
+Liquid::Template.register_tag('codemirror', Jekyll::CodeMirror)
--- assets/js/core.js
+++ assets/js/core.js
@@ -1,5 +1,17 @@
 $(function () {
-   $(".codemirror").each(function () {
-      console.log(this);
-   });
+  $(".codemirror").each(function () {
+    var lang = $(this).data("lang");
+
+    var map = {
+      js: "javascript",
+      html: "htmlmixed",
+      smarty: "smarty"
+    };
+
+    CodeMirror.fromTextArea(this, {
+      mode: map[lang],
+      readonly: true,
+      viewportMargin: Infinity
+    });
+  });
 });
