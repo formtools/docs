7f87d05de6e22cb504ca73a2115060117f3e65ee
Ben Keen <ben.keen@gmail.com>
2016-12-30 17:20:23 -0800

    layout update for live site compatibility

--- gruntfile.js
+++ gruntfile.js
@@ -9,12 +9,12 @@ module.exports = function(grunt) {
 
 	grunt.initConfig(config);
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-template');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-shell');
 
-  // starts the Jekyll server
-  grunt.registerTask("start", ["shell:start"]);
+	// starts the Jekyll server
+	grunt.registerTask("start", ["shell:start"]);
 };
